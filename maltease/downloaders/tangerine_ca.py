7d030be3031946d50f7d8788a11b39e5a71e650f
Guillaume Boudreau <guillaume@pommepause.com>
2017-02-04 21:25:25 -0500

    Bugfix: ofxclient' account.download() returns a StringIO

--- maltease/downloaders/tangerine_ca.py
+++ maltease/downloaders/tangerine_ca.py
@@ -20,4 +20,4 @@ class TangerineCa(OFXDownloader):
             account = ofx_config.account(account_number)
             ofxdata = account.download(days=30)
             with open("data/%s_%s.ofx" % (self.get_institution_code(), account_number), "w") as ofx_file:
-                print(ofxdata, file=ofx_file)
+                print(ofxdata.getvalue(), file=ofx_file)
